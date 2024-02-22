82b2f964513bd9fd45234536552f1b40f4f1d73c
nihar ranjan mohanty <nihar.m@secqureone.com>
2024-02-22 15:35:59 +0530

    all edge cases cleared

--- resourceapp/urls.py
+++ resourceapp/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('api/create-instance/', create_ec2_instance, name='create_instance'),
     path('api/create-elastic-ip-and-allocate-ec2/',create_elastic_ip_and_allocate_ec2),
+    path('api/get-all-instances/',get_all_instances)
 ]
